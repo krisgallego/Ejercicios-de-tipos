92f30deaa8308976d473faf8a5f625aa253bb7f9
krisgallego <krisgallego7@gmail.com>
2026-04-27 15:48:39 -0500

    feat: Exercise 1 finished

--- src/exercise1.ts
+++ src/exercise1.ts
@@ -2,12 +2,21 @@
 
 type TaskStatus = "pendiente" | "en_progreso" | "finalizada";
 
-type Task = {
+type Task = {   
     id: number;
     description: string;
     isComplete: boolean;
     status: TaskStatus
 }
+
 function getTaskProgressOrPending(tasks: Task[]) {
     return tasks.filter((task: Task) => task.status !== "finalizada")
 }
+
+const tareas: Task[] = [
+    { id: 1, description: "Estudiar", isComplete: false, status: "pendiente" },
+    { id: 2, description: "Trabajar", isComplete: true, status: "finalizada" },
+    { id: 3, description: "Entrenar", isComplete: false, status: "en_progreso" },
+];
+
+console.log("Ejercicio 1:", getTaskProgressOrPending(tareas));
