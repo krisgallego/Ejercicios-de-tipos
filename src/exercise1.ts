b9f870c5654b2d238bca0524d5064614e6c2a20d
krisgallego <krisgallego7@gmail.com>
2026-04-27 15:35:58 -0500

    exercise 1 finished

---
+++ src/exercise1.ts
@@ -0,0 +1,13 @@
+// Ejericio 1
+
+type TaskStatus = "pendiente" | "en_progreso" | "finalizada";
+
+type Task = {
+    id: number;
+    description: string;
+    isComplete: boolean;
+    status: TaskStatus
+}
+function getTaskProgressOrPending(tasks: Task[]) {
+    return tasks.filter((task: Task) => task.status !== "finalizada")
+}
