420c0a9ef731670cae1c0638254f99084bd4487a
krisgallego <krisgallego7@gmail.com>
2026-04-28 09:02:40 -0500

    feat: Exercise 6 finished

--- src/exercise6.ts
+++ src/exercise6.ts
@@ -1,2 +1,37 @@
 // Ejercicio 6
 
+type Calificacion = {
+    estudianteId: number;
+    categoria: "tareas" | "quices" | "examen";
+    nota: number;
+};
+
+function promedioPorCategoria(arr: Calificacion[], id: number) {
+    let res = {
+        tareas: { total: 0, count: 0 },
+        quices: { total: 0, count: 0 },
+        examen: { total: 0, count: 0 },
+    };
+
+    arr.forEach(calificaciones => {
+        if (calificaciones.estudianteId === id) {
+            res[calificaciones.categoria].total += calificaciones.nota;
+            res[calificaciones.categoria].count++;
+        }
+    });
+
+    return {
+        tareas: res.tareas.total / (res.tareas.count || 1),
+        quices: res.quices.total / (res.quices.count || 1),
+        examen: res.examen.total / (res.examen.count || 1),
+    };
+}
+
+const calificaciones: Calificacion[] = [
+    { estudianteId: 1, categoria: "tareas", nota: 3 },
+    { estudianteId: 2, categoria: "tareas", nota: 5 },
+    { estudianteId: 1, categoria: "quices", nota: 3 },
+    { estudianteId: 2, categoria: "tareas", nota: 1 },
+];
+
+console.log("Ejercicio 6:", promedioPorCategoria(calificaciones, 1));
